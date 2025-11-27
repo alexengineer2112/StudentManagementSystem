11477ffd3d719334be291e31da82142c62cb7606
unknown <alexjjnixon@gmail.com>
2025-11-27 17:02:37 +0530

    Added Student Management System project

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Student{
+    String name;
+    int age;
+    double cgpa;
+    public Student(String name,int age,double cgpa)
+    {
+        this.name = name;
+        this.age = age;
+        this.cgpa = cgpa;
+    }
+}
+
+    
